ff4b8280f84e35b7eec436e089f1efb11f36ee55
Carlos Rodriguez <124581108+cjrodriguez98@users.noreply.github.com>
2024-03-18 13:14:12 +0100

    Fixed documentation in calculateMCS.py

--- gMCSpy/calculateMCS.py
+++ gMCSpy/calculateMCS.py
@@ -42,7 +42,7 @@ def calculateMCS(
     Optional arguments:
         - **rxnToForce:** List of reactions to be considered in the MCS problem. The list is expected to be of reaction names. Default value is an empty list, which means that all reactions will be considered. e.g. rxnToForce = ["R1", "R2", "R3"] will only consider reactions R1, R2 and R3 as possible intervention points. Only ["R1"], ["R2"], ["R3"], ["R1", "R2"], ["R1", "R3"], ["R2", "R3"] and ["R1", "R2", "R3"] could be solutions.
 
-        - **rxnToKnockOut:** List of reactions that you want to be part of the MCS solutions. The list is expected to be of reaction names. Default value is an empty list, which means that no reactions will be forced to be part of the MCS solutions. e.g. rxnToKnockOut = ["R1", "R2", "R3"] will force the MCS solutions to contain reactions R1, R2 and R3. Only ["R1", "R2", "R3"] could be a solution.
+        - **rxnToKnockOut:** List of reactions that you want to be part of the MCS solutions. The list is expected to be of reaction names. Default value is an empty list, which means that no reactions will be forced to be part of the MCS solutions. e.g. rxnToKnockOut = ["R1", "R2", "R3"] will force the MCS solutions to contain at leat one reaction from the list.
         
         - **targetB:** Desired activity level of the metabolic task to be disrupted. Default value is 1e- 3.
 
